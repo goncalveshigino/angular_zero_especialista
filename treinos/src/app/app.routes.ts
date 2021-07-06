6fe8c0ad41316b7a2fe698b4eaab4a7275166256
goncalveshigino <goncalveshigino20@gmail.com>
2021-07-06 07:52:47 +0100

    rotas filhas

--- treinos/src/app/app.routes.ts
+++ treinos/src/app/app.routes.ts
@@ -4,12 +4,25 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
+import { UsuarioNovoComponent } from './components/usuario/usuario-novo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalheComponent } from './components/usuario/usuario-detalhe.component';
+
 
 
 const app_routes: Routes = [
 
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
+    {
+        path: 'usuario/:id',
+        component: UsuarioComponent,
+        children: [
+            { path: 'novo', component: UsuarioNovoComponent },
+            { path: 'editar', component: UsuarioEditarComponent },
+            { path: 'detalhe', component: UsuarioDetalheComponent },
+            { path: '**', pathMatch: 'full', redirectTo: 'novo'}
+        ]
+    },
     { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
