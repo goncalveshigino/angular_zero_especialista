a807cbdc8827b3d5200a66e7028fdd48ee11c00a
goncalveshigino <goncalveshigino20@gmail.com>
2021-07-08 13:25:03 +0100

    feito

--- formulario/karma.conf.js
+++ formulario/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fireChat'),
+      dir: require('path').join(__dirname, './coverage/formulario'),
       subdir: '.',
       reporters: [
         { type: 'html' },
